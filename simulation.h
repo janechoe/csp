60401576cfbcfd9f8a015bf5b6264f8a4cdc39da
kg1642 <kg1642@nyu.edu>
2016-05-07 22:45:06 +0400

    simulations added

---
+++ simulation.h
@@ -0,0 +1,22 @@
+#ifndef SIMULATION_H_INCLUDED
+#define SIMULATION_H_INCLUDED
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include "Agent.h"
+
+class Simulation {
+    int init_agents(int);
+    vector <Agent> agents;
+};
+
+int init_agents(max_agents){
+    for (int i=0; int <max_agents; i++){
+        Agent single_agent;
+        single_agent.setvalues(x,y,vx,vy,m,r);
+        agents.pushback(agent);
+    }
+}
+
+#endif // SIMULATION_H_INCLUDED
